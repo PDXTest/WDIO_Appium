35674ccdf05e123be17802afc526c73673ee7a5c
Chandrakant Bhirud <chandubhirud@yahoo.com>
2023-02-16 14:02:18 +0000

    test1

--- steps/login.ts
+++ steps/login.ts
@@ -3,6 +3,8 @@ import loginpage from "../pages/loginPage";
 
 const lp = new loginpage();
 
+//test
+
 Given(/^Login applcaition with (.+) and (.+)$/, async(user, pwd)=>{
     await lp.login(user, pwd);  
     setTimeout: 500000
