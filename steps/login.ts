894796da0b38dd8f96f29efab31014453d83e7ab
Chandrakant Bhirud <chandubhirud@yahoo.com>
2023-02-16 15:23:34 +0000

    ram

--- steps/login.ts
+++ steps/login.ts
@@ -3,7 +3,8 @@ import loginpage from "../pages/loginPage";
 
 const lp = new loginpage();
 
-//test test
+//test test test
+//test
 
 Given(/^Login applcaition with (.+) and (.+)$/, async(user, pwd)=>{
     await lp.login(user, pwd);  
